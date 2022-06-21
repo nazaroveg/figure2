212141d18f8d92a69a96b0bc52072b7d7d4570b9
NazarovEG <nazaroveg@yandex.ru>
2022-06-21 20:41:55 +0300

    провал 2

--- figure2.cpp
+++ figure2.cpp
@@ -9,8 +9,19 @@ class triangle  //треугольник
 public:
 	triangle() {}
 	triangle(int, int, int, int, int, int){}
+	triangle(int, int, int, int, int){}
+	triangle(int, int){}
 	
-
+	triangle* set_all(triangle* full)
+	{
+		this->corner_a = corner_a;
+		this->corner_b = corner_b;
+		this->corner_c = corner_c;
+		this->Length_a = Length_a;
+		this->Length_b = Length_b;
+		this->Length_c = Length_c;
+		return  &*full;
+	}
 
 	std::string get_name() const
 	{
@@ -36,11 +47,20 @@ protected:
 class Right_triangle : public triangle  // прямоугольный треугольник  один 90 гр
 {
 public:
-	
+	triangle* set_all(triangle* full)
+	{
+		this->corner_a = corner_a;
+		this->corner_b = corner_b;
+		this->corner_c = corner_c;
+		this->Length_a = Length_a;
+		this->Length_b = Length_b;
+		this->Length_c = Length_c;
+		return  &*full;
+	}
 	
 	Right_triangle(int, int, int, int, int)
 	{
-		corner_a, corner_b, Length_a, Length_b, Length_c;
+		corner_a, corner_b, corner_c = 90, Length_a, Length_b, Length_c;
 	}
 	std::string const name = "прямоугольный треугольник:";
 	
@@ -58,8 +78,20 @@ protected:
 class Isosceles_triangle : public triangle  //Равнобедренный треугольник 2 ст =
 {
 public:
-	
-	
+	Isosceles_triangle(int, int, int, int)
+	{
+		corner_c = corner_a, corner_b, Length_c = Length_a, Length_b;
+	}
+	triangle* set_all(triangle* full)
+	{
+		this->corner_a = corner_a;
+		this->corner_b = corner_b;
+		this->corner_c = corner_c;
+		this->Length_a = Length_a;
+		this->Length_b = Length_b;
+		this->Length_c = Length_c;
+		return  &*full;
+	}
 	
 	 std::string const name =  "Равнобедренный треугольник:" ;
 	
@@ -78,8 +110,20 @@ protected:
 class Equilateral_triangle : public triangle //Равносторонний треугольник  всё равно
 {
 public:
-	
-	
+	Equilateral_triangle(int, int)
+	{
+		corner_c = corner_b = corner_a, Length_c = Length_b = Length_a;
+	}
+	triangle* set_all(triangle* full)
+	{
+		this->corner_a = corner_a;
+		this->corner_b = corner_b;
+		this->corner_c = corner_c;
+		this->Length_a = Length_a;
+		this->Length_b = Length_b;
+		this->Length_c = Length_c;
+		return  &*full;
+	}
 	std::string name = "Равносторонний треугольник:";
 
 	std::string get_name() const
@@ -151,10 +195,10 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 
-	triangle tr {10 20 30 50 90 80 };
-	//Right_triangle Rt {10,20,90,10};
-	//Isosceles_triangle It {10,20,10,40,50};
-	Equilateral_triangle Et{};
+	triangle tr {10, 20, 30, 50, 90, 80 };
+	Right_triangle Rt {10,20,90,10};
+	Isosceles_triangle It {10,20,10,40,50};
+	Equilateral_triangle Et{40 , 40};
 	Quadrilateral Ql;
 	Rectangle Re;
 	Square Sq;
@@ -162,12 +206,12 @@ int main()
 	Rhombus Rh;
 
 	
-	tr.print_info(&tr, tr.get_name());
+	//tr.print_info(&tr, tr.get_name());
 	//Rt.print_info(&Rt, *&Rt.get_name());
 	//It.print_info(&It, It.get_name());
 	//Et.print_info(&Et, Et.get_name());
-	
-
+	tr.print_info(tr.set_all(&tr), tr.get_name());
+	Rt.print_info(Rt.set_all(&Rt), Rt.get_name());
 
 }
 
